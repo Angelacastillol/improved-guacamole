8f775135f1858efef6a817c1f80b3a49ce82194c
Angela castillo <116675009+Angelacastillol@users.noreply.github.com>
2025-09-15 22:45:17 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+// Obtenemos el contenedor
+const container = document.getElementById('name-container');
+
+// Creamos un h1 con el texto "Angela"
+//<h1> "Angela"</h1>;
+
+// Agregamos el h1 al contenedor
+container.appendChild(heading);
